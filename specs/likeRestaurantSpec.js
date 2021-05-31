2f8cf41cd03907b9cdc9d0a64f1759af12d2647f
dhonadwi <dhonadwi2@gmail.com>
2021-05-31 11:28:29 +0700

    automate ke 1 ada 3

--- specs/likeRestaurantSpec.js
+++ specs/likeRestaurantSpec.js
@@ -1,3 +1,4 @@
+import FavoriteRestaurantIdb from '../src/scripts/data/restaurants-idb';
 import LikeButtonInitiator from './../src/scripts/utils/like-button-initiator';
 
 describe('Liking A Restaurant', () => {
@@ -11,5 +12,31 @@ describe('Liking A Restaurant', () => {
     });
     expect(document.querySelector('[aria-label="like this restaurant"]'))
       .toBeTruthy();
+  });
+  it('should not show the unlike button when the restaurant has not been liked before', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this restaurant"]'))
+      .toBeFalsy();
+  });
+  it('should be able to like the restaurant', async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+    const restaurant = await FavoriteRestaurantIdb.getRestaurant(1);
+
+    expect(restaurant).toEqual({ id: 1 });
+    FavoriteRestaurantIdb.deleteRestaurant(1);
   });
 });
